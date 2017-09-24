7c133f5e7635d7533283269cb1eaa1ec2f67b5fd
zhangzheng1019 <447590461@qq.com>
2017-09-24 14:16:00 +0800

    使用 express 4.x swig+stylus复制页面 https://m.jimu.com/aboutus

---
+++ 3.express4x swig stylus/index.js
@@ -0,0 +1,24 @@
+// 加载express模块
+var express = require('express');
+var path = require("path");
+// 生成express实例
+var app = express();
+// 设定express实例参数
+// 设定访问端口
+app.set('port',process.env.PORT || 1234);
+// 视图存放目录
+app.set('view', path.join(__dirname, 'view'));
+// 网页模板引擎
+app.set('view engine', 'swig');
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router());
+
+// 设定静态文件目录（比如图片/js/css地址）
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'));
+
